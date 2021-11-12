38d2c9af545fa54a2468d4765807668c0e254389
Moeezulhaq <moeez.ulhaq@eurustechnologies.com>
2021-11-12 11:32:13 -0800

    tsting

--- Tag.sh
+++ Tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gittag=$(git log -1 --pretty=%h)
-docker build -t $gittag .
+docker build -t jenkins .
 docker tag jenkins:$gittag public.ecr.aws/y2a9o9h4/jenkins:$gittag
 docker push public.ecr.aws/y2a9o9h4/jenkins:$gittag
-aws ecr-public get-login-password --region us-east-1  
+aws ecr-public get-login-password --region us-east-1 
 docker login --username AWS --password-stdin public.ecr.aws/y2a9o9h4
