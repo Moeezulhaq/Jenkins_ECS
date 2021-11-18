ee1091c3f4991177a892e2ec6ad77fd08723d449
Moeezulhaq <moeez.ulhaq@eurustechnologies.com>
2021-11-17 16:02:36 -0800

    checking

--- vars/SlackNotification.groovy
+++ vars/SlackNotification.groovy
@@ -3,7 +3,7 @@ def call (noti) {
         slackSend channel: 'moeez_testing', message: "Please Find status of Job status- ${currentBuild.currentResult} Build Name-${env.JOB_NAME} Build Number-${env.BUILD_NUMBER} Build URL-${env.BUILD_URL}"
 }
 
-// def call () {
+def call () {
         
-//     sh "echo hello world"
-// }
+    sh "echo hello world"
+}
