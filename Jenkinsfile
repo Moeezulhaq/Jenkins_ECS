b5260350ec4e7f5e0c9fa9082a0a28f905324b57
Moeezulhaq <moeez.ulhaq@eurustechnologies.com>
2021-11-18 15:00:13 -0800

    checking

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,4 @@
-@Library("shared-library") _
+@Library("shared-library@main") _
 pipeline {
   agent any
   parameters {
@@ -41,7 +41,7 @@ pipeline {
     always{
 
     script{
-        SlackNotification.notification(currentBuild.currentResult)
+        SlackNotification.notification()
       }
     }
   }  
--- vars/SlackNotification.groovy
+++ vars/SlackNotification.groovy
@@ -1,11 +1,10 @@
 #!/usr/bin/env groovy
- 
-def notification (String value) {
-      
-       slackSend channel: 'moeez_testing', message: "Please Find status of Job status- ${currentBuild.currentResult} Build Name-${env.JOB_NAME} Build Number-${env.BUILD_NUMBER} Build URL-${env.BUILD_URL}"
+
+def notification () {
+    slackSend channel: 'moeez_testing', message: "Please Find status of Job status- ${currentBuild.currentResult} Build Name-${env.JOB_NAME} Build Number-${env.BUILD_NUMBER} Build URL-${env.BUILD_URL}"
 }
- 
+
 def hello() {
-      
-   sh "echo hello world"
+        
+    sh "echo hello world"
 }
