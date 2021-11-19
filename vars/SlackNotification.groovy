5301e6fbcefbf9207bec6765fc4ac9efa6171b17
Moeezulhaq <moeez.ulhaq@eurustechnologies.com>
2021-11-18 16:48:34 -0800

    changes

--- vars/SlackNotification.groovy
+++ vars/SlackNotification.groovy
@@ -1,8 +1,9 @@
 
-// def notification(String value){
+def notification( ){
         
-//         slackSend channel: 'moeez_testing', message: "Please Find status of Job status- ${currentBuild.currentResult} Build Name-${env.JOB_NAME} Build Number-${env.BUILD_NUMBER} Build URL-${env.BUILD_URL}"
-// }
+    slackSend channel: 'moeez_testing', message: "Please Find status of Job status- ${currentBuild.currentResult} Build Name-${env.JOB_NAME} Build Number-${env.BUILD_NUMBER} Build URL-${env.BUILD_URL}"
+
+}
 
 def hello(){
         
