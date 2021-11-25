4f9eaddad5b7beaf1f94971473f9d6a6dbeda355
Moeezulhaq <moeez.ulhaq@eurustechnologies.com>
2021-11-25 10:00:58 -0800

    checking:changing desired count to 20

---
+++ vars/SlackNotification.groovy
@@ -0,0 +1,9 @@
+
+def notification( ){
+        
+    slackSend channel: 'moeez_testing', message: "Please Find status of Job status- ${currentBuild.currentResult} Build Name-${env.JOB_NAME} Build Number-${env.BUILD_NUMBER} Build URL-${env.BUILD_URL}"
+
+}
+
+
+
